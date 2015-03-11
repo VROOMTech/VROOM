39d505c217003620e4f10583e7fdd86f982c92a6
prudio <prudio@uw.edu>
2015-03-11 15:03:50 -0700

    create load page, update app.js, add main logo

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 //////////// modules ///////////////
-var express = require('express');
-var app = express();
+var express     = require('express');
+var stylus      = require('stylus');
+var nib         = require('nib');
+var bodyParser  = require('body-parser');
+var app         = express();
 
 ///////////// port /////////////////
 var port = process.env.PORT || 8080;
+
+//////////// logger ////////////////
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+///////// Jade and Stylus //////////
+function compile (str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .use(nib());
+}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }        
+));
+app.use(express.static(__dirname + '/public'));
+
 ///////// http requests ////////////
 app.get('/', function (req, res) {
-    res.send('<h1>VROOM VROOM!</h1>');
+    //res.send('<h1>VROOM VROOM!</h1>');
+    res.render('index', 
+        {title: 'VROOM'});
 });
 
 var server = app.listen(port, function () {
