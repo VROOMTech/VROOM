7f08a0647bba7d80168914da6ff4515ca18767e8
PhilipRudio <prudio@uw.edu>
2015-05-05 16:53:58 -0700

    add routes and partials

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res) {
+    res.render('index', {
+        title: 'VROOM!'
+    });
+};
+
+exports.partials = function(req, res) {
+    var name = req.params.name;
+    res.render('partials/' + name);
+};
