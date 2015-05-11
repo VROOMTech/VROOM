defc083e30d4da3bd8875b49cc9ab8885a89fe31
PhilipRudio <prudio@uw.edu>
2015-05-11 13:02:53 -0700

    update MainController

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,32 +1,30 @@
-//(function() {
-    var app = angular.module('vroomApp');
+var app = angular.module('vroomApp');
 
-    var MainController = function($scope, $interval, $location) {
-        var decrementCountdown = function() {
-            $scope.countdown -= 1;
-            if($scope.countdown < 1) {
-                $scope.start();
-            }
-        };   
+var MainController = function($scope, $interval, $location) {
+    var decrementCountdown = function() {
+        $scope.countdown -= 1;
+        if($scope.countdown < 1) {
+            $scope.start();
+        }
+    };   
 
-        var countdownInterval = null;
-        var startCountdown = function() {
-            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
-        };
+    var countdownInterval = null;
+    var startCountdown = function() {
+        countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
         
-        $scope.start = function() {
-            if(countdownInterval) {
-                $interval.cancel(countdownInterval);
-                $scope.countdown = null;
-            }
-            $location.path("/fuelGauge");
-        };
-
-        $scope.countdown = 5;
-        startCountdown();
-
+    $scope.start = function() {
+        if(countdownInterval) {
+            $interval.cancel(countdownInterval);
+            $scope.countdown = null;
+        }
+        $location.path("/confirm-location");
+        //$location.path("/fuel-gauge");
     };
 
-    app.controller("MainController", MainController);
+    $scope.countdown = 5;
+    startCountdown();
 
-//}());
+};
+
+app.controller("MainController", MainController);
