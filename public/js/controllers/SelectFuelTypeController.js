285994fe4d87332d5bc3cc8d5371cf906e805355
Philip Rudio <prudio@uw.edu>
2015-05-26 12:58:20 -0700

    add 'select gas type' view

---
+++ public/js/controllers/SelectFuelTypeController.js
@@ -0,0 +1,41 @@
+var app = angular.module('vroomApp');
+
+var SelectFuelTypeController = function($scope, $location) {
+
+    var keywords = {"yes": true, 
+        "yeah": true, 
+        "sure": true, 
+        "yup": true};
+
+    var recognizer = new webkitSpeechRecognition(); // jshint ignore:line
+    recognizer.continuous = true;
+    recognizer.interimResults = true;
+    recognizer.lang = "en";
+    recognizer.onresult = function(e) {
+        if(e.results.length) {
+            var lastResultIndex = e.results.length - 1;
+            var words = e.results[lastResultIndex][0].transcript.split(" ");
+            $scope.receivedWords = words[words.length - 1];
+            console.log('words is: ' + words);
+            words.forEach(function(element) {
+                console.log(element + ' in keywords is ' + keywords[element]);
+                if(keywords[element]) {
+                    console.log(element + " was said"); 
+                    // route to gas station list
+                    $scope.$apply(function() {
+                        $location.path("/station-list");   
+                        recognizer.stop();
+                    });
+
+                }
+            });
+        }
+    };
+
+    $scope.listen = function() {
+        console.log('listening');
+        recognizer.start();
+    };
+};
+
+app.controller("SelectFuelTypeController", SelectFuelTypeController);
