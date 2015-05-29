f52a3e68fa9fd4619db24644c455eecaab719a16
Philip Rudio <prudio@uw.edu>
2015-05-29 09:37:54 -0700

    clean myGasFeed.js

--- public/js/modules/myGasFeed.js
+++ public/js/modules/myGasFeed.js
@@ -2,14 +2,22 @@
     var myGasFeed = function($http) {
 
         var getNearbyGasStations = function(lat, lng, dist, fuelType, sortBy) {
-            return $http.get("https://api.mygasfeed.com/stations/radius/" + lat + "/" + lng + "/" + dist + "/" + fuelType + "/" + sortBy + "/apikey.json")
+            return $http.get("https://api.mygasfeed.com/stations/radius/" 
+                + lat + "/" 
+                + lng + "/" 
+                + dist + "/" 
+                + fuelType + "/" 
+                + sortBy 
+                + "/apikey.json")
                         .then(function(response) {
                             return response.data;
                         });
         };
 
         var getGasStationDetails = function(stationId) {
-            return $http.get("http://api.mygasfeed.com/stations/details/" + stationId + "/apikey.json")
+            return $http.get("http://api.mygasfeed.com/stations/details/" 
+                + stationId 
+                + "/apikey.json")
 
             .then(function(response) {
                 return response.data;
