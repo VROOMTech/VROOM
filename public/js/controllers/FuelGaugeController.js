167420d0e614ac70779f8c9b9d9b974f5e215a27
Philip Rudio <prudio@uw.edu>
2015-06-03 23:55:27 -0700

    add animated mic and speak label

--- public/js/controllers/FuelGaugeController.js
+++ public/js/controllers/FuelGaugeController.js
@@ -133,6 +133,29 @@ var FuelGaugeController = function($scope, $location, $timeout) {
         "dismiss": true};
 
     var recognizer = new webkitSpeechRecognition(); // jshint ignore:line
+    recognizer.onstart = function() {
+        $scope.isListening = true;
+        var foot = document.getElementsByClassName('footer')[0];
+        var h1 = document.createElement("span");
+        h1.className = 'speak-now';
+        var text = document.createTextNode("speak now");
+        h1.appendChild(text);
+        foot.appendChild(h1);
+
+
+        $scope.shrinkMic();
+        console.log('speech started');
+    };
+
+    recognizer.onend = function() {
+        $scope.isListening = false;
+        console.log('speech ended');
+        var footer = document.getElementsByClassName('footer')[0];
+        var speakLabel = document.getElementsByClassName('speak-now')[0];
+        console.log(speakLabel);
+        footer.removeChild(speakLabel);
+
+    };
     recognizer.continuous = true;
     recognizer.interimResults = true;
     recognizer.lang = "en";
@@ -161,9 +184,48 @@ var FuelGaugeController = function($scope, $location, $timeout) {
         }
     };
 
+    var micToggle = true;
+    var micIsBig = micToggle ? "mic-icon big" : "mic-icon small";
+
+    $scope.animateMic = function() {
+        micToggle = !micToggle;
+        micIsBig = micToggle ? "mic-icon big" : "mic-icon small";
+        var mic = document.getElementsByClassName('mic-icon')[0]; 
+        mic.className = micIsBig;
+    };
+
+    $scope.enlargeMic = function() {
+        if($scope.isListening) {
+            $scope.animateMic();
+            $timeout(function() {
+                $scope.shrinkMic();
+            }, 600); 
+        } 
+    };
+
+    $scope.shrinkMic = function() {
+        if($scope.isListening) {
+            $scope.animateMic();
+            $timeout(function() {
+                $scope.enlargeMic(); 
+            }, 600); 
+
+        }
+    };
+
     $scope.listen = function() {
+        if($scope.isListening) {
+            $scope.isListening = false;
+            recognizer.stop();
+        }
         console.log('listening');
         recognizer.start();
+    };
+
+    $scope.activate = function() {
+        console.log('ACTIVATE!');
+        console.log('update1');
+        $scope.listen();
     };
 
     $scope.fadeIcon = function() {
